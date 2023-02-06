aa76f55f838436fea9a1dbfeb2da0fd4b10af0ca
dev-jiwonchoi <jiwon.choi@nanugo.kr>
2023-02-07 03:02:05 +0900

    Feat: Add simple todo-list

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,2 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Todo(props) {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemsLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text>{props.text}</Text>
+        <View style={styles.circle}></View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  itemsLeft: {},
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "orange",
+  },
+  itemText: {
+    maxWidth: '80%'
+  },
+  circle: {
+    width: 12,
+    height: 12
+  },
+});
--- pages/to-do.tsx
+++ pages/to-do.tsx
@@ -1,13 +1,62 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Todo from "../components/Todo";
 
 export default function ToDos() {
+  const [todo, setTodo] = useState(null);
+  const [todoItems, setTodoItems] = useState([]);
+
+  const createTodo = () => {
+    Keyboard.dismiss();
+    setTodoItems([...todoItems, todo]);
+    setTodo(null);
+  };
+
+  const removeTodo = (index) => {
+    let itemsCopy = [...todoItems];
+    itemsCopy.splice(index, 1);
+    setTodoItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.toDoWrapper}>
         <Text style={styles.Title}>Today</Text>
-        <View style={styles.items}></View>
+        <View style={styles.items}>
+          {todoItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => removeTodo(index)}>
+                <Todo text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
       </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.inputWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"test"}
+          onChangeText={(input) => setTodo(input)}
+          value={todo}
+        />
+        <TouchableOpacity onPress={() => createTodo()}>
+          <View style={styles.addInputWrapper}>
+            <Text style={styles.addInput}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -15,15 +64,43 @@ export default function ToDos() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
   },
   toDoWrapper: {
     paddingTop: 80,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   Title: {
     fontSize: 24,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   items: {},
+  inputWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: "80%",
+  },
+  addInputWrapper: {
+    width: 50,
+    height: 50,
+    backgroundColor: "white",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addInput: {},
 });
